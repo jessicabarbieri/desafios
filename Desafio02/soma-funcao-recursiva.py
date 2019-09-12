355d95ee3e4cb0d6aad9d1971d248fb74b96c0ee
jessicabarbieri <46322515+jessicabarbieri@users.noreply.github.com>
2019-09-12 10:32:42 -0300

    Update soma-funcao-recursiva.py

--- Desafio02/soma-funcao-recursiva.py
+++ Desafio02/soma-funcao-recursiva.py
@@ -1,26 +1,25 @@
-
 #soma com função recursiva
-# def soma_recursiva(n1, n2=0):
-#     try:
-#         res = n1[n2]
-#         n2 -= -1
-#         return res + soma_recursiva(n1,n2)
+def soma_recursiva(n1, n2=0):
+    try:
+        res = -n1[n2]
+        n2 -= -1
+        return -(res - soma_recursiva(n1,n2))
         
-#     except:
-#         return 0
-
-# n1 = int(input('Digite o primeiro numero: '))
-# n2 = int(input('Digite o segundo numero: '))
-
-# print(soma_recursiva([n1,n2]))
-
-def soma_recursiva(n1, n2 = 0):
-    for i in n1:
-        n2 -= -i
-        
-    return n2
+    except:
+        return 0
 
 n1 = int(input('Digite o primeiro numero: '))
 n2 = int(input('Digite o segundo numero: '))
 
-print(soma_recursiva([n1, n2]))
+print(soma_recursiva([n1,n2]))
+
+# def soma_recursiva(n1, n2 = 0):
+#     for i in n1:
+#         n2 -= -i
+        
+#     return n2
+
+# n1 = int(input('Digite o primeiro numero: '))
+# n2 = int(input('Digite o segundo numero: '))
+
+# print(soma_recursiva([n1, n2]))
