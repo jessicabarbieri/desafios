82042b00c633539eed2a82389f853e0f8a999043
jessicabarbieri <46322515+jessicabarbieri@users.noreply.github.com>
2019-09-12 16:45:15 -0300

    Update soma-funcao-recursiva.py

--- Desafio02/soma-funcao-recursiva.py
+++ Desafio02/soma-funcao-recursiva.py
@@ -9,12 +9,27 @@
 # y = int(input('Digite o segundo numero: '))
 # soma(x,y)
 
-def soma_bitwise(x, y):
-    while y:
-        x, y = (x ^ y), (x & y) << 1
-    return x
+# def soma_bitwise(x, y):
+#     while (y != 0):
+#         z = x & y
+#         x = x ^ y
+#         y = z << 1
+#     return x
+# x = int(input('Digite o primeiro numero: '))
+# y = int(input('Digite o segundo numero: '))
+# print(soma_bitwise(x, y))
 
-x = int(input('Digite o primeiro numero: '))
-y = int(input('Digite o segundo numero: '))
 
-print(soma_bitwise(x, y)) 
+# import sys
+# sys.setrecursionlimit(100000)
+def soma_bitwise(a, b):
+    if b == 0:
+        return a
+    sum = a ^ b
+    carry = (a & b) << 1
+    return soma_bitwise(sum, carry)
+
+a = int(input('Digite o primeiro numero: '))
+b = int(input('Digite o segundo numero: '))
+
+print(soma_bitwise(a, b)) 
